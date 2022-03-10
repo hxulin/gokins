285acd1229f8cee6e3fb25fae6a1470de9e83d9d
hxulin <1455936807@qq.com>
2022-03-10 13:11:24 +0800

    新增编译脚本

---
+++ build.cmd
@@ -0,0 +1,43 @@
+@echo off
+
+REM 设置版本号
+set version=1.0.1
+
+REM windows amd64
+go build
+rename gokins.exe gokins-%version%-windows-amd64.exe
+
+REM linux amd64
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=amd64
+go build
+rename gokins gokins-%version%-linux-amd64
+
+REM linux arm
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=arm
+go build
+rename gokins gokins-%version%-linux-arm
+
+REM linux arm64
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=arm64
+go build
+rename gokins gokins-%version%-linux-arm64
+
+REM macOS amd64
+SET CGO_ENABLED=0
+SET GOOS=darwin
+SET GOARCH=amd64
+go build
+rename gokins gokins-%version%-darwin-amd64
+
+REM macOS arm64
+SET CGO_ENABLED=0
+SET GOOS=darwin
+SET GOARCH=arm64
+go build
+rename gokins gokins-%version%-darwin-arm64
